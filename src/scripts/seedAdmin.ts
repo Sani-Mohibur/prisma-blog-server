988008551ced00e9d96a7bdcbefed0e36cb36d48
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-05-26 04:40:27 +0600

    chore: comment out seedAdmin function and related code

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -1,49 +1,49 @@
-import { prisma } from "../lib/prisma";
-import { UserRole } from "../middlewares/auth.middleware";
+// import { prisma } from "../lib/prisma";
+// import { UserRole } from "../middlewares/auth.middleware";
 
-async function seedAdmin() {
-  try {
-    const adminData = {
-      name: "Farabi Sunny",
-      email: "farabisunny5@gmail.com",
-      password: "farabi1234",
-      role: UserRole.ADMIN,
-    };
+// async function seedAdmin() {
+//   try {
+//     const adminData = {
+//       name: "Farabi Sunny",
+//       email: "farabisunny5@gmail.com",
+//       password: "farabi1234",
+//       role: UserRole.ADMIN,
+//     };
 
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        email: "farabisunny5@gmail.com",
-      },
-    });
+//     const existingUser = await prisma.user.findUnique({
+//       where: {
+//         email: "farabisunny5@gmail.com",
+//       },
+//     });
 
-    if (existingUser) {
-      throw new Error("User already exists!");
-    }
+//     if (existingUser) {
+//       throw new Error("User already exists!");
+//     }
 
-    const signUpAdmin = await fetch(
-      "http://localhost:5000/api/auth/sign-up/email",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(adminData),
-      },
-    );
+//     const signUpAdmin = await fetch(
+//       "http://localhost:5000/api/auth/sign-up/email",
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(adminData),
+//       },
+//     );
 
-    if (signUpAdmin.ok) {
-      await prisma.user.update({
-        where: {
-          email: adminData.email,
-        },
-        data: {
-          emailVerified: true,
-        },
-      });
-    }
-  } catch (error: any) {
-    console.error(error.message);
-  }
-}
+//     if (signUpAdmin.ok) {
+//       await prisma.user.update({
+//         where: {
+//           email: adminData.email,
+//         },
+//         data: {
+//           emailVerified: true,
+//         },
+//       });
+//     }
+//   } catch (error: any) {
+//     console.error(error.message);
+//   }
+// }
 
-seedAdmin();
+// seedAdmin();
